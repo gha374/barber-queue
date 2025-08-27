78ce7c220801616117d493303f1e9771baa50a75
gha374 <ghassenelheoui@gmail.com>
2025-08-27 11:49:29 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+// app/layout.tsx
+import './globals.css'
+import { ReactNode } from 'react'
+
+export const metadata = {
+  title: 'Barber Queue',
+  description: 'Booking app for barber',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
